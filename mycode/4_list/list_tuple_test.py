aa068cc89f3c76150e4a5c1feee6797584a06e09
mysoyul <vega2k_@naver.com>
2021-11-17 18:01:14 +0900

    module import

--- mycode/4_list/list_tuple_test.py
+++ mycode/4_list/list_tuple_test.py
@@ -17,17 +17,22 @@ my_list1[2] = 100
 print(my_list1[0:3])
 
 #set - 중복 허용 않함, 순서가 유지되지 않음
+print('set ---')
+print(my_list1)
 my_set = set(my_list1)
 print(type(my_set))
 print(my_set)
+print('set ---')
 
 #typel - read only list
+print('tuple -----')
 my_tuple = (10,20,30)
 my_tuple2 = tuple()
 print(type(my_tuple))
 print(my_tuple)
 #TypeError: 'tuple' object does not support item assignment
 #my_tuple[0] = 50
+print('tuple -----')
 
 num1 = (3)
 num2 = (3,)
@@ -42,14 +47,32 @@ print(a)
 print(b)
 
 #문자열 타입 List 선언
+print('str ----')
 cat_list = list('cat')
 print(cat_list)
 
 birth_day = "2021/11/16"
 birth_list = birth_day.split("/")
 print(birth_list)
+
 print('2021' in birth_list)
 print('2021' not in birth_list)
 
 if '2021' not in birth_list:
     print('not found')
+
+my_str  = 'hello world python'
+print(my_str.count('o'))
+print(len(my_str))
+print(my_str.find('w'))
+
+str_list = ['java','python','scalar']
+result = " ".join(str_list)
+print(result[0:5])
+print(result.upper())
+print(result.lower())
+print(result.title())
+print('str ----')
+
+my_str = "hello" * 4
+print(my_str)
--- mycode/6_data_structure/dictionary_test.py
+++ mycode/6_data_structure/dictionary_test.py
@@ -1,7 +1,7 @@
 """
 dictionary는 key:value 쌍으로 이루어진 데이터를 저장하는 자료구조
 """
-my_dict = dict()
+my_dict = dict() # {}
 my_dict = {'name': '홍길동', 'age': 20}
 print(type(my_dict))
 
@@ -9,6 +9,8 @@ print(type(my_dict))
 print(my_dict['name'])
 #dict에 새로운 key와 value를 추가
 my_dict['addr'] = '충주'
+
+print(my_dict)
 
 #print(my_dict['name1'])
 result = my_dict.get('name1')
@@ -20,5 +22,12 @@ else:
 if "name" in my_dict:
     print('name key가 있습니다.')
 
-for key,value in my_dict.items():
+for key, value in my_dict.items():
     print(key,value)
+
+#int(), float(), str(),
+#list(), tuple(), set(), dict()
+# list [ ] - 값 추가 허용
+# tuple ( ) - 값 추가 허용 않함
+# set { } - 중복허용 않함, 순서유지 않됨
+# dict { } - key값으로 value 조회
